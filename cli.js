2b3cf62b0a3056427804c4183139e192466fa9ae
qiwei <wei.qi@leyantech.com>
2025-04-14 18:48:23 +0800

    fix

--- cli.js
+++ cli.js
@@ -14,6 +14,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-const { program } = require('./lib/program');
+const { program } = require('./lib/cli/programWithTestStub');
 program.parse(process.argv);
--- index.js
+++ index.js
@@ -13,5 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+const minimumMajorNodeVersion = 14;
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split('.');
+const [major] = [+semver[0]];
 
-module.exports = require('playwright-core');
+if (major < minimumMajorNodeVersion) {
+  console.error(
+      'You are running Node.js ' +
+      currentNodeVersion +
+      '.\n' +
+      `Playwright requires Node.js ${minimumMajorNodeVersion} or higher. \n` +
+      'Please update your version of Node.js.'
+  );
+  process.exit(1);
+}
+
+module.exports = require('./lib/inprocess');
